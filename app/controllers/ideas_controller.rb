5365b3b3bde1f21c7f84da65fd3df0ef2984c4d4
KenChukanit <chonlasek.c@gmail.com>
2021-02-01 10:27:49 -0800

    update ideas controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,8 @@
 class IdeasController < ApplicationController
 
+    before_action   :authenticate_user!, except: [:index, :show]
+    before_action  :find_idea, only: [:show, :edit, :update, :destroy]
+
     def new
         @idea = Idea.new
     end
@@ -7,7 +10,7 @@ class IdeasController < ApplicationController
 
     def create
         @idea = Idea.new idea_params
-        # @idea.user = current_user
+        @idea.user = current_user
         if @idea.save
             flash[:notice] = "Idea created."
             redirect_to idea_path(@idea.id)
@@ -15,6 +18,37 @@ class IdeasController < ApplicationController
             render :new
         end
     end
+
+    def index
+        @ideas = Idea.all.order(created_at: :desc)
+    end
+
+    def show
+    
+    end
+
+    def edit
+    end
+
+    def update
+        if @idea.update idea_params
+            flash[:notice] = "Idea updated."
+            redirect_to idea_path(@idea.id)
+        else 
+            render :edit
+        end
+    end
+
+    def destroy
+        if @idea.destroy
+            flash[:alert] = "Idea deleted."
+        redirect_to ideas_path
+        else  
+            render :show
+        end
+    end
+
+
 
     private
 
