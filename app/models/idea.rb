4170eac00c8ccd6acc58ae720a0768168bb50f3f
KenChukanit <chonlasek.c@gmail.com>
2021-02-01 11:24:06 -0800

    add review model and controller

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,7 @@
 class Idea < ApplicationRecord
     belongs_to :user, optional: true
+    has_many :reviews, dependent: :destroy
+
     validates :title, presence: true
     validates :body, presence: true
 
